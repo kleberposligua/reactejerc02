efffff6341b89f7726aea0be7f9322002fa7e287
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2022-02-04 12:49:53 -0500

    Hasta video 41

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+//import "./App.css";
+import Expenses from './components/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Gastos Carro",
+      amount: 294.67,
+      date: new Date(2021, 2, 11),
+    },
+    {
+      id: 2,
+      title: "Gastos de Internet",
+      amount: 394.67,
+      date: new Date(2020, 10, 18),
+    },
+    {
+      id: 3,
+      title: "Gastos de Salud",
+      amount: 194.67,
+      date: new Date(2019, 6, 28),
+    },
+    {
+      id: 4,
+      title: "Gastos de Vivienda",
+      amount: 494.67,
+      date: new Date(2022, 2, 15),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
